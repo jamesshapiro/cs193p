86c5134c20c77be54ce700ca8c0bc456077416c0
James Shapiro <jamesshapiro@gmail.com>
2018-04-11 00:14:06 -0700

    oval drawing example

---
+++ PlayingCard/PlayingCard/PlayingCardView.swift
@@ -0,0 +1,33 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by James Shapiro on 4/10/18.
+//  Copyright © 2018 James Shapiro. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    override func draw(_ rect: CGRect) {
+//        if let context = UIGraphicsGetCurrentContext() {
+// Strokes but doesn't fill
+//            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+//            context.setLineWidth(5.0)
+//            UIColor.green.setFill()
+//            UIColor.red.setStroke()
+//            context.strokePath()
+//            context.fillPath()
+//        }
+        
+// After setting view as "redraw", draws a filled in circle
+//        let path = UIBezierPath()
+//        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+//        path.lineWidth = 5.0
+//        UIColor.green.setFill()
+//        UIColor.red.setStroke()
+//        path.stroke()
+//        path.fill()
+    }
+}
