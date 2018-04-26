ac5a680026bbeec2d656241b2fa3f85a7ba03bcf
James Shapiro <jamesshapiro@gmail.com>
2018-04-25 23:48:46 -0700

    begin adding comments to Set Game

--- SetGame/SetGame/SetGamePlayerVsComputerViewController.swift
+++ SetGame/SetGame/SetGamePlayerVsComputerViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class SetGamePlayerVsComputerViewController: UIViewController {
     private var setGame: SetCardGame! = nil
-    private var numShown = 0
+    // Number of cards currently on the board. Gets increased to 12
+    // at the beginning of the game during setup.
+    private var numberOfCardsOnBoard = 0
+    // Cards that the player has tapped on and selected.
     private var selected = [UIButton]()
+    
+    // In player vs. computer mode, the computer uses this timer
+    // to determine how much time the player has left to make a
+    // move before the computer steps in and wins the round
     private weak var timer: Timer?
+    
+    // Boolean registering whether the computer or the human
+    // identified the set. We use this to decide whether to
+    // color a matched set green (if the human won) or orange
+    // (otherwise)
     private var computerWonRound = false
     
+    // Facial expression used to personify the computer to the player.
     @IBOutlet weak var computerFacialExpression: UILabel!
     @IBOutlet weak var dealThreeButton: UIButton!
     @IBOutlet var cardButtons: [UIButton]!
@@ -22,22 +35,43 @@ class SetGamePlayerVsComputerViewController: UIViewController {
     @IBOutlet weak var computerScore: UILabel!
     @IBOutlet weak var gameResult: UILabel!
     
-    var computerFace: ComputerFace = .thinking {
+    // Depending on how long it has been since the last round, the computer is
+    // either "thinking" about it's next move, grinning in anticipation of
+    // its impending move, or celebrating or mourning the result of the
+    // just-concluded round. In reality, the computer "thinks" up its move
+    // instantaneously when a semi-randomly set timer expires.
+    enum ComputerFace: String {
+        case thinking = "🤔"
+        case anticipating = "😁"
+        case laughing = "😂"
+        case crying = "😢"
+    }
+
+    var computerEmotionalState: ComputerFace = .thinking {
         didSet {
-            computerFacialExpression.text = computerFace.rawValue
+            computerFacialExpression.text = computerEmotionalState.rawValue
         }
     }
     
-    func setTimer(interval: TimeInterval? = nil) {
-        let thonkingTime = Double(arc4random_uniform(UInt32(240 / numShown))) + 5
-        let thinkingTime = interval ??  thonkingTime
-        timer = Timer.scheduledTimer(withTimeInterval: thinkingTime, repeats: false) {_ in
-            switch self.computerFace {
+    func setTimer(predeterminedDelay: TimeInterval? = nil) {
+        let randomDelay = Double(arc4random_uniform(UInt32(240 / numberOfCardsOnBoard))) + 5
+        let timeComputerSpendsThinking = predeterminedDelay ??  randomDelay
+        timer = Timer.scheduledTimer(withTimeInterval: timeComputerSpendsThinking, repeats: false) {_ in
+            switch self.computerEmotionalState {
+            // After "contemplating" its next move, the computer has identified a set and
+            // is now giving the human player a few seconds to move. If the player does
+            // not select a set in that time, the computer will win the round and remove
+            // its set from the board.
             case .thinking:
-                self.computerFace = .anticipating
-                self.setTimer(interval: 3.0)
+                self.computerEmotionalState = .anticipating
+                self.setTimer(predeterminedDelay: 3.0)
+            // After giving the human player a few seconds to move (during which time the
+            // player did NOT successfully select a set), the computer is now swooping
+            // in to make its move and win the round.
             case .anticipating:
-                self.computerFace = .laughing
+                self.computerEmotionalState = .laughing
+                // If there is a set of cards on the board: select them, increase the
+                // computer's score, and highlight the computer-identified set in orange
                 if let setOfCards = self.setGame.findASetOfCards() {
                     self.selected = setOfCards.map { self.cardButtons[$0] }
                     self.setGame.updatePlayerVsComputerScore(with: self.indicesOfSelectedButtons, forHuman: false)
@@ -46,34 +80,52 @@ class SetGamePlayerVsComputerViewController: UIViewController {
                         $0.isEnabled = false
                     }
                     self.updateViewFromModel()
+                    // Otherwise, there are two possibilities:
+                    // (1.) Every card in the deck has been drawn and there are no
+                    // more remaining sets left to be identified.
+                    // (2.) Not every card has been drawn, but the only way more sets
+                    // can be identified is by increasing the number of cards on the
+                    // board by drawing three, one or more times.
+                    // In either case, the score does not change and no sets are
+                    // removed from the board. However, in case 1, the game is over
+                    // and the player learns who won.
                 } else {
                     if self.setGame.isOutOfCards {
                         if self.setGame.score > self.setGame.computerScore {
                             self.gameResult.text = "You won!"
-                            self.computerFace = .crying
+                            self.computerEmotionalState = .crying
                         } else if self.setGame.score < self.setGame.computerScore {
                             self.gameResult.text = "You lost!"
-                            self.computerFace = .laughing
+                            self.computerEmotionalState = .laughing
                         } else {
                             self.gameResult.text = "You tied."
-                            self.computerFace = .thinking
+                            self.computerEmotionalState = .thinking
                         }
                         self.gameResult.isHidden = false
                         self.timer?.invalidate()
                         break
                     }
                 }
-                
-                self.setTimer(interval: 3.0)
+                self.setTimer(predeterminedDelay: 3.0)
+            // After celebrating its victory in the last round, the computer
+            // now contemplates its next move for a random amount of time.
+            // If the player has not pressed any buttons since the end of the
+            // last round, the computer clears away the matched set and replaces
+            // it with three new cards (if possible).
             case .laughing:
-                self.setTimer(interval: thonkingTime)
+                self.computerEmotionalState = .thinking
+                self.setTimer(predeterminedDelay: randomDelay)
                 if self.selected.count == 3 {
                     self.updateViewFromModel(indicesReceivingNewCards: self.indicesOfSelectedButtons)
                     self.cardButtons.forEach {
                         $0.isEnabled = true
                     }
                 }
-                self.computerFace = .thinking
+            // After mourning its defeat in the last round, the computer
+            // now contemplates its next move for a random amount of time.
+            // If the player has not pressed any buttons since the end of
+            // the last round, the computer clears away the selected set
+            // and replaces it with three new cards (if possible).
             case .crying:
                 if self.selected.count == 3 {
                     self.updateViewFromModel(indicesReceivingNewCards: self.indicesOfSelectedButtons)
@@ -81,8 +133,8 @@ class SetGamePlayerVsComputerViewController: UIViewController {
                         $0.isEnabled = true
                     }
                 }
-                self.computerFace = .thinking
-                self.setTimer(interval: thonkingTime)
+                self.computerEmotionalState = .thinking
+                self.setTimer(predeterminedDelay: randomDelay)
             }
         }
     }
@@ -94,25 +146,18 @@ class SetGamePlayerVsComputerViewController: UIViewController {
         startNewGame()
     }
     
-    enum ComputerFace: String {
-        case thinking = "🤔"
-        case anticipating = "😁"
-        case laughing = "😂"
-        case crying = "😢"
-    }
-    
     private var indicesOfSelectedButtons: [Int] {
         return selected.map { cardButtons.index(of: $0)! }
     }
     
     private var cardsSlotsAreAllInUse: Bool {
-        return numShown == cardButtons.count
+        return numberOfCardsOnBoard == cardButtons.count
     }
     
 
     
     @IBAction func startNewGame() {
-        numShown = 0
+        numberOfCardsOnBoard = 0
         selected = [UIButton]()
         setGame = SetCardGame()
         cardButtons.forEach { $0.isHidden = true }
@@ -132,8 +177,8 @@ class SetGamePlayerVsComputerViewController: UIViewController {
         if setGame.cardsFormASet(with: indicesOfSelectedButtons) {
             updateViewFromModel(indicesReceivingNewCards: indicesOfSelectedButtons)
         } else {
-            numShown += 3
-            updateViewFromModel(indicesReceivingNewCards: Array(numShown-3..<numShown))
+            numberOfCardsOnBoard += 3
+            updateViewFromModel(indicesReceivingNewCards: Array(numberOfCardsOnBoard-3..<numberOfCardsOnBoard))
         }
     }
     
@@ -148,9 +193,9 @@ class SetGamePlayerVsComputerViewController: UIViewController {
             }
             if selected.count == 3, setGame.cardsFormASet(with: indicesOfSelectedButtons) {
                 timer?.invalidate()
-                self.computerFace = .crying
+                self.computerEmotionalState = .crying
                 setGame.updatePlayerVsComputerScore(with: indicesOfSelectedButtons, forHuman: true)
-                setTimer(interval: 3.0)
+                setTimer(predeterminedDelay: 3.0)
             }
         } else {
             // three cases: (1.) user selects a different card (select card)
@@ -195,7 +240,7 @@ class SetGamePlayerVsComputerViewController: UIViewController {
             }
         }
         
-        for index in cardButtons[..<numShown].indices where !selected.contains(cardButtons[index]) {
+        for index in cardButtons[..<numberOfCardsOnBoard].indices where !selected.contains(cardButtons[index]) {
             cardButtons[index].layer.borderColor = UIColor.gray.cgColor
         }
         
